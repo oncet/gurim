70dd52ec75115ea3f13767713ff6c947803bb423
Camilo <camilorivera86@gmail.com>
2019-07-01 00:36:26 -0300

    Pass secondary menu to the homepage view

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
 $context = Timber::context();
 
 $context['posts'] = new Timber\PostQuery();
+$context['secondary_menu'] = new \Timber\Menu('secondary-menu');
 
 Timber::render(['index.twig'], $context);
