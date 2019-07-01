461fb5173b322c9afbf0cdc9cb13294997bfde6c
Camilo <camilorivera86@gmail.com>
2019-07-01 00:29:57 -0300

    Added attachments support

--- functions.php
+++ functions.php
@@ -59,3 +59,25 @@ if(!post_type_exists('product')) {
         ]);
     });
 }
+
+/**
+ * Remove default attachments instance and register a custom one.
+ */
+add_filter('attachments_default_instance', '__return_false');
+
+function gurim_attachments($attachments) {
+  $attachments->register( 'gurim_attachments', [
+    'post_type' => ['product'],
+    'position' => 'side',
+    'priority' => 'high',
+    'filetype' => 'image',
+    'append' => false,
+    'button_text' => __( 'Attach Files', 'attachments' ),
+    'modal_text' => __( 'Attach', 'attachments' ),
+    'router' => 'upload',
+    'fields' => []
+  ]);
+}
+
+add_action('attachments_register', 'gurim_attachments');
+
