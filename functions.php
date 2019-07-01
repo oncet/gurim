73566a8cbb822fa056803e1b3258fdf03ff98499
Camilo <camilorivera86@gmail.com>
2019-07-01 00:32:45 -0300

    Added bootstrap files

--- functions.php
+++ functions.php
@@ -110,6 +110,8 @@ function gurim_context($context) {
  */
 function gurim_styles_and_scripts() {
     wp_enqueue_style('gurim', get_stylesheet_uri());
+    wp_enqueue_style('bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.min.css');
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js');
 }
 
 add_action('wp_enqueue_scripts', 'gurim_styles_and_scripts');
