9a99179b0721ad14c4301aaf15c177058f4eb653
Camilo <camilorivera86@gmail.com>
2019-07-20 11:48:43 -0300

    Added default value to custom colors options

--- includes/context.php
+++ includes/context.php
@@ -6,8 +6,8 @@
 function gurim_context($context) {
     $context['blog_name'] = get_bloginfo('name');
     $context['blog_logo'] = wp_get_attachment_image_url(get_theme_mod('custom_logo'), 'full');
-    $context['background_color'] = get_theme_mod('gurim_background_color');
-    $context['secondary_menu_link_color'] = get_theme_mod('gurim_secondary_menu_link_color');
+    $context['background_color'] = get_theme_mod('gurim_background_color', '#fffef6');
+    $context['secondary_menu_link_color'] = get_theme_mod('gurim_secondary_menu_link_color', '#636363');
     $context['site'] = new \Timber\Site();
     $context['primary_menu'] = new \Timber\Menu('primary-menu');
     $context['secondary_menu'] = new \Timber\Menu('secondary-menu');
