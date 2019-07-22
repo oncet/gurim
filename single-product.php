d1351acd0b065183b590358ce8567af229c7454d
Camilo <camilorivera86@gmail.com>
2019-07-21 21:41:26 -0300

    Use large size image in product detail page

--- single-product.php
+++ single-product.php
@@ -3,5 +3,6 @@
 $context = Timber::context();
 
 $context['post'] = new Timber\Post();
+$context['thumbnail'] = $context['post']->thumbnail->src('large');
 
 Timber::render(['single-product.twig'], $context);
