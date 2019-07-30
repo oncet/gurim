514dc3270ab57fe36c68b5bc8e4b32ea5b7ecfa2
Camilo <camilorivera86@gmail.com>
2019-07-29 23:50:47 -0300

    Removed unnecessary variable

--- single-product.php
+++ single-product.php
@@ -3,6 +3,5 @@
 $context = Timber::context();
 
 $context['post'] = new Timber\Post();
-$context['thumbnail'] = $context['post']->thumbnail->src('large');
 
 Timber::render(['single-product.twig'], $context);
