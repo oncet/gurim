55c40e989da8d6b2ea66c0f5edf7a3a28e2b3840
Camilo <camilorivera86@gmail.com>
2019-08-12 00:29:11 -0300

    Working on the products archive page

--- includes/features.php
+++ includes/features.php
@@ -5,3 +5,9 @@
  */
 add_theme_support('post-thumbnails');
 add_theme_support('custom-logo');
+
+if(!get_option('medium_crop')) {
+    add_option('medium_crop', '1');
+} else {
+    update_option('medium_crop', '1');
+}
