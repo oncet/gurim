99cd7b0b6c487943ae7babbccd7cfe965884ca2d
Camilo <camilorivera86@gmail.com>
2019-08-17 13:07:45 -0300

    Add proper class to archive menu item

--- functions.php
+++ functions.php
@@ -7,3 +7,25 @@ require_once( __DIR__ . '/includes/customizable-options.php');
 require_once( __DIR__ . '/includes/menus.php');
 require_once( __DIR__ . '/includes/context.php');
 require_once( __DIR__ . '/includes/enqueue-scripts.php');
+
+add_action('nav_menu_css_class', 'add_current_nav_class', 10, 2 );
+
+function add_current_nav_class($classes, $item) {
+    // Getting the current post details
+    global $post;
+
+    // Getting the post type of the current post
+    $current_post_type = get_post_type_object(get_post_type($post->ID));
+    $current_post_type_slug = $current_post_type->rewrite['slug'];
+
+    // Getting the URL of the menu item
+    $menu_slug = strtolower(trim($item->url));
+
+    // If the menu item URL contains the current post types slug add the current-menu-item class
+    if ($current_post_type_slug && strpos($menu_slug, $current_post_type_slug) !== false) {
+       $classes[] = 'active';
+    }
+
+    // Return the corrected set of classes to be added to the menu item
+    return $classes;
+}
