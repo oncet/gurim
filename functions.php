ac29f5a8a09d75cf11ae3de51e41e7e5691a1b97
Camilo <camilorivera86@gmail.com>
2019-08-18 18:30:01 -0300

    Include custom post type in category page query

--- functions.php
+++ functions.php
@@ -9,7 +9,6 @@ require_once( __DIR__ . '/includes/context.php');
 require_once( __DIR__ . '/includes/enqueue-scripts.php');
 
 add_action('nav_menu_css_class', 'add_current_nav_class', 10, 2 );
-
 function add_current_nav_class($classes, $item) {
     // Getting the current post details
     global $post;
@@ -32,4 +31,12 @@ function add_current_nav_class($classes, $item) {
 
     // Return the corrected set of classes to be added to the menu item
     return $classes;
+}
+
+add_action('parse_query', 'add_custom_post_types');
+function add_custom_post_types() {
+    if( is_category() && !is_admin() ) {
+        set_query_var( 'post_type', ['post', 'product'] );
+    }
+    return;
 }
