9fa0bef6d76d97c02ed1cf75dd56d233571d95b8
Camilo <camilorivera86@gmail.com>
2019-09-05 21:30:09 -0300

    Removed secondary color option

--- includes/context.php
+++ includes/context.php
@@ -8,7 +8,6 @@ function gurim_context($context) {
     $context['blog_title'] = wp_title('&raquo;', false, 'right');
     $context['blog_logo'] = wp_get_attachment_image_url(get_theme_mod('custom_logo'), 'full');
     $context['background_color'] = get_theme_mod('gurim_background_color', '#fffef6');
-    $context['secondary_menu_link_color'] = get_theme_mod('gurim_secondary_menu_link_color', '#636363');
     $context['site'] = new \Timber\Site();
 
     $menus = get_nav_menu_locations();
--- includes/customizable-options.php
+++ includes/customizable-options.php
@@ -15,17 +15,5 @@ function gurim_customize_register($wp_customize) {
             'section'   => 'colors'
         ]
     ));
-
-    $wp_customize->add_setting('gurim_secondary_menu_link_color', [ 'default' => '#636363' ]);
-
-    $wp_customize->add_control(new WP_Customize_Color_Control(
-        $wp_customize,
-        'gurim_secondary_menu_link',
-        [
-            'settings'  => 'gurim_secondary_menu_link_color',
-            'label'     => 'Secondary menu link',
-            'section'   => 'colors'
-        ]
-    ));
 }
 add_action('customize_register', 'gurim_customize_register');
