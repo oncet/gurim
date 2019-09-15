48396df4388f1e03a9dec418583e7d31d53ae6aa
Camilo <camilorivera86@gmail.com>
2019-09-15 05:09:09 -0300

    Removed obsolete customizable option

--- functions.php
+++ functions.php
@@ -3,7 +3,6 @@
 require_once( __DIR__ . '/includes/dependency-checks.php');
 require_once( __DIR__ . '/includes/features.php');
 require_once( __DIR__ . '/includes/custom-post-types.php');
-require_once( __DIR__ . '/includes/customizable-options.php');
 require_once( __DIR__ . '/includes/menus.php');
 require_once( __DIR__ . '/includes/context.php');
 require_once( __DIR__ . '/includes/enqueue-scripts.php');
