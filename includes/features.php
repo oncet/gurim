62f81462197ef5e0bb9e7b2ae9a4fc4f557fc7ab
Camilo <camilorivera86@gmail.com>
2019-09-15 05:07:44 -0300

    Enable custom background support using body class

--- includes/features.php
+++ includes/features.php
@@ -5,6 +5,7 @@
  */
 add_theme_support('post-thumbnails');
 add_theme_support('custom-logo');
+add_theme_support('custom-background', ['default-color' => 'fffef6']);
 
 if(!get_option('medium_crop')) {
     add_option('medium_crop', '1');
