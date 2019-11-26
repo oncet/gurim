cb9587703c3f66507ce1acace7f12a1cb2b91ded
Camilo <camilorivera86@gmail.com>
2019-11-25 23:00:14 -0300

    Avoid animations stacking

--- js/secondary-menu.js
+++ js/secondary-menu.js
@@ -1,6 +1,7 @@
 jQuery(function() {
   jQuery('.menu-item-has-children:not(.current-menu-parent):not(.current-menu-item)').hover(function(event) {
     var subMenu = jQuery(this).find('.sub-menu');
+    subMenu.stop();
     if (event.type === 'mouseenter') {
       subMenu.slideDown(150);
     }
