d6e24073f8bd3e089ed1ad65c75341670dea349f
Camilo <camilorivera86@gmail.com>
2019-11-26 22:45:18 -0300

    Added tiny delay to menu animations

--- js/secondary-menu.js
+++ js/secondary-menu.js
@@ -1,12 +1,21 @@
 jQuery(function() {
-  jQuery('.menu-item-has-children:not(.current-menu-parent):not(.current-menu-item)').hover(function(event) {
-    var subMenu = jQuery(this).find('.sub-menu');
+  var timeout;
+  function toggle(subMenu, type) {
     subMenu.stop();
-    if (event.type === 'mouseenter') {
+    if (type === 'mouseenter') {
       subMenu.slideDown(150);
     }
-    if (event.type === 'mouseleave') {
+    if (type === 'mouseleave') {
       subMenu.slideUp(150);
     }
-  });
+  }
+  jQuery('.menu-item-has-children:not(.current-menu-parent):not(.current-menu-item)')
+    .hover(function(event) {
+      clearTimeout(timeout);
+      var type = event.type;
+      var subMenu = jQuery(this).find('.sub-menu');
+      timeout = setTimeout(function() {
+        toggle(subMenu, type);
+      }, 250);
+    });
 });
