37cdef11172462cbc68dc7ed54bef4c102abe388
Camilo <camilorivera86@gmail.com>
2020-12-10 23:44:07 -0300

    display posts

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 
-$context = Timber::context();
+$context = Timber::context();global $paged;
+
+if (!isset($paged) || !$paged){
+    $paged = 1;
+}
+
 
 $context['products'] = new Timber\PostQuery([
-    'post_type' => 'product',
-    'posts_per_page' => 6,
-    'meta_key' => 'featured'
+    'posts_per_page' => get_option('posts_per_page'),
+    'paged' => $paged,
 ]);
 
 Timber::render(['index.twig'], $context);
